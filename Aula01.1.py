1dfed3a9e6626e676d12c0d16ed8d23e7957d54c
mateus <mateus.pereira4@gmail.com>
2022-05-25 21:53:36 -0300

    Exemplos

---
+++ Aula01.1.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+
+# back-end
+
+def diminuir():
+    if int(lb1['text']) > -10:
+        lb1['text'] = int(lb1['text']) - 1
+    else:
+        pass
+
+
+def aumentar():
+    if int(lb1['text']) < 10:
+        lb1['text'] = int(lb1['text']) + 1
+    else:
+        pass
+
+
+# janela
+root = Tk()
+
+# widgets
+lb1 = Label(root, text='0', padx=20, pady=10, width=2)
+bt1 = Button(root, text='-', command=diminuir, padx=10, pady=10, width=2)
+bt2 = Button(root, text='+', command=aumentar, padx=10, pady=10, width=2)
+
+# layout
+bt1.grid(row=0, column=0)
+lb1.grid(row=0, column=1)
+bt2.grid(row=0, column=2)
+
+# executa
+root.mainloop()
--- Aula01.py
+++ Aula01.py
@@ -1,30 +1,52 @@
 # Aula 01 será apresentado os elementos básicos de uma GUI
 from tkinter import *
 
+
+# BACK-END
+def imprimir():
+    lb2['text'] = ent1.get()
+    print(ent1.get())
+
+
 # ELEMENTOS:
 # 1) WINDOW
 root = Tk()
 root.title('App GUI')
-#root.geometry('300x300')
+root.geometry('200x130')
 
 # 2) FRAME
-frm1 = Frame(root)
 
 # 3) WIDGET
-lb1 = Label(frm1, text='Digite aqui:')
-lb2 = Label(frm1)
-ent1 = Entry(frm1)
-btn1 = Button(frm1, text='Click aqui!')
-btn2 = Button(frm1, text='Sair', command=quit)
+lb1 = Label(root, text='Digite aqui:')
+lb2 = Label(root)
+ent1 = Entry(root)
+btn1 = Button(root, text='Click aqui!', command=imprimir)
+btn2 = Button(root, text='Sair', command=quit)
 
 # 4) LAYOUT
-# 4.1) PACK
-frm1.pack()
+# 4.1) PLACE
 
-# 4.2) PLACE
 lb1.place(x=10, y=10)
+ent1.place(x=10, y=30)
+btn1.place(x=10, y=60)
+btn2.place(x=110, y=60)
+lb2.place(x=10, y=100)
 
+# 4.1) PACK
+'''
+lb1.pack()
+ent1.pack()
+btn1.pack()
+btn2.pack()
+lb2.pack()
+'''
 # 4.3) GRID
-
+'''
+lb1.grid(row=0, column=0)
+ent1.grid(row=1, column=0)
+btn1.grid(row=1, column=1)
+btn2.grid(row=2, column=0)
+lb2.grid(row=3, column=0)
+'''
 # EXECUTA O WINDOW
 root.mainloop()
