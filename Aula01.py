6d826e9f60c2042e8178d221e5b045f6608166cb
mateus <mateus.pereira4@gmail.com>
2022-05-29 10:21:48 -0300

    Exemplos 1

--- Aula01.py
+++ Aula01.py
@@ -25,28 +25,28 @@ btn2 = Button(root, text='Sair', command=quit)
 
 # 4) LAYOUT
 # 4.1) PLACE
-
+'''
 lb1.place(x=10, y=10)
 ent1.place(x=10, y=30)
 btn1.place(x=10, y=60)
 btn2.place(x=110, y=60)
 lb2.place(x=10, y=100)
-
-# 4.1) PACK
 '''
+# 4.1) PACK
+
 lb1.pack()
 ent1.pack()
-btn1.pack()
-btn2.pack()
+btn1.pack(fill='both', expand=1, pady=5, padx=5)
+btn2.pack(fill='both', expand=1, pady=5, padx=5)
 lb2.pack()
-'''
+
 # 4.3) GRID
 '''
 lb1.grid(row=0, column=0)
 ent1.grid(row=1, column=0)
-btn1.grid(row=1, column=1)
-btn2.grid(row=2, column=0)
-lb2.grid(row=3, column=0)
+btn1.grid(row=2, column=0)
+btn2.grid(row=3, column=0)
+lb2.grid(row=4, column=0)
 '''
 # EXECUTA O WINDOW
 root.mainloop()
