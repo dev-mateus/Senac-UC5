76f90a47615b2fd193218b264ad15a6f9c91116f
mateus <mateus.pereira4@gmail.com>
2022-05-29 22:05:46 -0300

    Exemplos 5

--- Aula01.5.py
+++ Aula01.5.py
@@ -1,48 +1,61 @@
-# calculadora
 from tkinter import *
 
-root = Tk()
+janela = Tk()
+janela.title('Calculadora')
+janela.geometry('200x250')
 
-fr1 = Frame(root)
-fr2 = Frame(root, background='blue')
+ent1 = Entry(janela)
+ent1.grid(row=0, column=0, columnspan=5, sticky=W+E)
 
-lb1 = Label(fr1)
-en1 = Entry(fr1)
+lb1 = Label(janela)
+lb1.grid(row=1, column=0, columnspan=5)
 
-bt1 = Button(fr2, text='1')
-bt2 = Button(fr2, text='2')
-bt3 = Button(fr2, text='3')
-bt4 = Button(fr2, text='4')
-bt5 = Button(fr2, text='5')
-bt6 = Button(fr2, text='6')
-bt7 = Button(fr2, text='7')
-bt8 = Button(fr2, text='8')
-bt9 = Button(fr2, text='9')
-
-fr1.pack()
-fr2.pack(fill='both', expand=1)
-
-lb1.pack()
-en1.pack()
-
-bt1.grid(row=0, column=0)
-bt2.grid(row=0, column=1)
-bt3.grid(row=0, column=2)
-bt4.grid(row=1, column=0)
-bt5.grid(row=1, column=1)
-bt6.grid(row=1, column=2)
+bt7 = Button(text='7', width=2, height=1)
 bt7.grid(row=2, column=0)
+
+bt8 = Button(text='8', width=2, height=1)
 bt8.grid(row=2, column=1)
+
+bt9 = Button(text='9', width=2, height=1)
 bt9.grid(row=2, column=2)
 
-'''
-bt1.pack(side='left', fill='both', expand=1)
-bt2.pack(side='left', fill='both', expand=1)
-bt3.pack(side='left', fill='both', expand=1)'''
-'''bt4
-bt5
-bt6
-bt7
-bt8
-bt9'''
-root.mainloop()
+bt4 = Button(text='4', width=2, height=1)
+bt4.grid(row=3, column=0)
+
+bt5 = Button(text='5', width=2, height=1)
+bt5.grid(row=3, column=1)
+
+bt6 = Button(text='6', width=2, height=1)
+bt6.grid(row=3, column=2)
+
+bt1 = Button(text='1', width=2, height=1)
+bt1.grid(row=4, column=0)
+
+bt2 = Button(text='2', width=2, height=1)
+bt2.grid(row=4, column=1)
+
+bt3 = Button(text='3', width=2, height=1)
+bt3.grid(row=4, column=2)
+
+bt0 = Button(text='0', width=2, height=1)
+bt0.grid(row=5, column=0)
+
+bt10 = Button(text='=', bg='gray', width=2, height=1)
+bt10.grid(row=5, column=1)
+
+bt11 = Button(text='*', width=2, height=1)
+bt11.grid(row=2, column=4)
+
+bt12 = Button(text='/', width=2, height=1)
+bt12.grid(row=3, column=4)
+
+bt13 = Button(text='+', width=2, height=1)
+bt13.grid(row=4, column=4)
+
+bt14 = Button(text='-', width=2, height=1)
+bt14.grid(row=5, column=4)
+
+bt14 = Button(text='.', width=2, height=1)
+bt14.grid(row=5, column=2)
+
+janela.mainloop()
