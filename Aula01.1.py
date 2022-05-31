db120392d0be3cca3c95e3b9e5f885f99cc1b301
mateus <mateus.pereira4@gmail.com>
2022-05-30 23:23:09 -0300

    Exemplos 1

--- Aula01.1.py
+++ Aula01.1.py
@@ -1,4 +1,5 @@
 # label interativo
+import keyword
 from tkinter import *
 
 
@@ -7,6 +8,7 @@ from tkinter import *
 def diminuir():
     if int(lb1['text']) > -10:
         lb1['text'] = int(lb1['text']) - 1
+
     else:
         pass
 
@@ -24,11 +26,14 @@ root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure(2, weight=1)
+root.bind('-', lambda event: diminuir())
+root.bind('+', lambda event: aumentar())
 
 # widgets
 lb1 = Label(root, text='0', padx=20, pady=10, width=3)
 bt1 = Button(root, text='-', command=diminuir, padx=10, pady=10, width=3)
 bt2 = Button(root, text='+', command=aumentar, padx=10, pady=10, width=3)
+
 
 # layout
 bt1.grid(row=0, column=0, sticky=NSEW)
