3f590b9cefb751ee96501320b88def528ee4fff7
mateus <mateus.pereira4@gmail.com>
2022-05-30 22:51:33 -0300

    Exemplos 1

--- Aula01.1.py
+++ Aula01.1.py
@@ -20,16 +20,20 @@ def aumentar():
 
 # janela
 root = Tk()
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(2, weight=1)
 
 # widgets
-lb1 = Label(root, text='0', padx=20, pady=10, width=2)
-bt1 = Button(root, text='-', command=diminuir, padx=10, pady=10, width=2)
-bt2 = Button(root, text='+', command=aumentar, padx=10, pady=10, width=2)
+lb1 = Label(root, text='0', padx=20, pady=10, width=3)
+bt1 = Button(root, text='-', command=diminuir, padx=10, pady=10, width=3)
+bt2 = Button(root, text='+', command=aumentar, padx=10, pady=10, width=3)
 
 # layout
-bt1.grid(row=0, column=0)
-lb1.grid(row=0, column=1)
-bt2.grid(row=0, column=2)
+bt1.grid(row=0, column=0, sticky=NSEW)
+lb1.grid(row=0, column=1, sticky=NSEW)
+bt2.grid(row=0, column=2, sticky=NSEW)
 
 # executa
 root.mainloop()
