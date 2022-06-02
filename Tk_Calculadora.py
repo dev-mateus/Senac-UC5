4801e5c3caaf46e0fd2d348b675bbb43e19ac28b
mateus <mateus.pereira4@gmail.com>
2022-06-02 14:14:03 -0300

    implementação da função limpa resultado

--- Tk_Calculadora.py
+++ Tk_Calculadora.py
@@ -9,9 +9,14 @@ def saida():
     lb1['text'] = eval(lb1['text'])
 
 
+def limpa():
+    lb1['text'] = ''
+
+
 janela = Tk()
 janela.title('Calculadora')
 janela.geometry('400x500')
+janela.bind('<Escape>', lambda event: limpa())
 
 janela.grid_rowconfigure(0, weight=1, minsize=50)
 janela.grid_rowconfigure(1, weight=1, minsize=50)
@@ -26,22 +31,22 @@ janela.grid_columnconfigure(3, weight=1, minsize=50)
 
 # widgets
 lb1 = Label(janela, font='Verdana 26')
-bt7 = Button(text='7', width=2, height=1, font='Verdana 26', command=lambda: resultado('7'))
-bt8 = Button(text='8', width=2, height=1, font='Verdana 26', command=lambda: resultado('8'))
-bt9 = Button(text='9', width=2, height=1, font='Verdana 26', command=lambda: resultado('9'))
-bt4 = Button(text='4', width=2, height=1, font='Verdana 26', command=lambda: resultado('4'))
-bt5 = Button(text='5', width=2, height=1, font='Verdana 26', command=lambda: resultado('5'))
-bt6 = Button(text='6', width=2, height=1, font='Verdana 26', command=lambda: resultado('6'))
-bt1 = Button(text='1', width=2, height=1, font='Verdana 26', command=lambda: resultado('1'))
-bt2 = Button(text='2', width=2, height=1, font='Verdana 26', command=lambda: resultado('2'))
-bt3 = Button(text='3', width=2, height=1, font='Verdana 26', command=lambda: resultado('3'))
-bt0 = Button(text='0', width=2, height=1, font='Verdana 26', command=lambda: resultado('0'))
-bt10 = Button(text='=', bg='gray', width=2, height=1, font='Verdana 26', command=saida)
-bt11 = Button(text='*', width=2, height=1, font='Verdana 26', command=lambda: resultado('*'))
-bt12 = Button(text='/', width=2, height=1, font='Verdana 26', command=lambda: resultado('/'))
-bt13 = Button(text='+', width=2, height=1, font='Verdana 26', command=lambda: resultado('+'))
-bt14 = Button(text='-', width=2, height=1, font='Verdana 26', command=lambda: resultado('-'))
-bt15 = Button(text='.', width=2, height=1, font='Verdana 26', command=lambda: resultado('.'))
+bt7 = Button(janela, text='7', width=2, height=1, font='Verdana 26', command=lambda: resultado('7'))
+bt8 = Button(janela, text='8', width=2, height=1, font='Verdana 26', command=lambda: resultado('8'))
+bt9 = Button(janela, text='9', width=2, height=1, font='Verdana 26', command=lambda: resultado('9'))
+bt4 = Button(janela, text='4', width=2, height=1, font='Verdana 26', command=lambda: resultado('4'))
+bt5 = Button(janela, text='5', width=2, height=1, font='Verdana 26', command=lambda: resultado('5'))
+bt6 = Button(janela, text='6', width=2, height=1, font='Verdana 26', command=lambda: resultado('6'))
+bt1 = Button(janela, text='1', width=2, height=1, font='Verdana 26', command=lambda: resultado('1'))
+bt2 = Button(janela, text='2', width=2, height=1, font='Verdana 26', command=lambda: resultado('2'))
+bt3 = Button(janela, text='3', width=2, height=1, font='Verdana 26', command=lambda: resultado('3'))
+bt0 = Button(janela, text='0', width=2, height=1, font='Verdana 26', command=lambda: resultado('0'))
+bt10 = Button(janela, text='=', bg='gray', width=2, height=1, font='Verdana 26', command=saida)
+bt11 = Button(janela, text='*', width=2, height=1, font='Verdana 26', command=lambda: resultado('*'))
+bt12 = Button(janela, text='/', width=2, height=1, font='Verdana 26', command=lambda: resultado('/'))
+bt13 = Button(janela, text='+', width=2, height=1, font='Verdana 26', command=lambda: resultado('+'))
+bt14 = Button(janela, text='-', width=2, height=1, font='Verdana 26', command=lambda: resultado('-'))
+bt15 = Button(janela, text='.', width=2, height=1, font='Verdana 26', command=lambda: resultado('.'))
 
 # layout
 lb1.grid(row=0, column=0, columnspan=5, sticky=EW)
