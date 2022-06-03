6eec744bae7fce0df0b5ab7b1125e42dbb5ed906
mateus <mateus.pereira4@gmail.com>
2022-06-03 10:53:40 -0300

    botão limpar e deletar

--- Tk_Calculadora.py
+++ Tk_Calculadora.py
@@ -12,7 +12,7 @@ def entrada(valor):
 
 def saida():
     ultimo_digito = lb1['text'][len(lb1['text']) - 1]
-    if ultimo_digito != '+' and ultimo_digito != '-' and ultimo_digito != '*' and ultimo_digito != '/':
+    if ultimo_digito != '+' and ultimo_digito != '-' and ultimo_digito != '*' and ultimo_digito != '/' and ultimo_digito != '.':
         res = eval(lb1['text'])
         lb1['text'] = str(res)
         lb1['fg'] = '#27990d'
@@ -24,12 +24,33 @@ def saida():
 def limpa():
     lb1['text'] = ''
 
+def limpa_ultimo():
+    lb1['text'] = lb1['text'][:-1]
+
 
 janela = Tk()
 janela.title('Calculadora')
 janela.geometry('400x500')
 janela.bind('<Escape>', lambda event: limpa())
 janela.bind('<Return>', lambda event: saida())
+janela.bind('<BackSpace>', lambda event: limpa_ultimo())
+janela.bind('0', lambda event:entrada('0') )
+janela.bind('1', lambda event:entrada('1') )
+janela.bind('2', lambda event:entrada('2') )
+janela.bind('3', lambda event:entrada('3') )
+janela.bind('4', lambda event:entrada('4') )
+janela.bind('5', lambda event:entrada('5') )
+janela.bind('6', lambda event:entrada('6') )
+janela.bind('7', lambda event:entrada('7') )
+janela.bind('8', lambda event:entrada('8') )
+janela.bind('9', lambda event:entrada('9') )
+janela.bind('+', lambda event:entrada('+') )
+janela.bind('-', lambda event:entrada('-') )
+janela.bind('/', lambda event:entrada('/') )
+janela.bind('*', lambda event:entrada('*') )
+janela.bind('.', lambda event:entrada('.') )
+janela.bind(')', lambda event:entrada(')') )
+janela.bind('(', lambda event:entrada('(') )
 
 janela.grid_rowconfigure(0, weight=1, minsize=50)
 janela.grid_rowconfigure(1, weight=1, minsize=50)
@@ -60,6 +81,10 @@ bt12 = Button(janela, text='/', width=2, height=1, font='Verdana 26', command=la
 bt13 = Button(janela, text='+', width=2, height=1, font='Verdana 26', command=lambda: entrada('+'))
 bt14 = Button(janela, text='-', width=2, height=1, font='Verdana 26', command=lambda: entrada('-'))
 bt15 = Button(janela, text='.', width=2, height=1, font='Verdana 26', command=lambda: entrada('.'))
+bt16 = Button(janela, text='Del', width=2, height=1, font='Verdana 26', command=lambda: limpa_ultimo())
+bt17 = Button(janela, text='C', width=2, height=1, font='Verdana 26', command=lambda: limpa())
+bt18 = Button(janela, text='(', width=2, height=1, font='Verdana 26', command=lambda: entrada('('))
+bt19 = Button(janela, text=')', width=2, height=1, font='Verdana 26', command=lambda: entrada(')'))
 
 # layout
 lb1.grid(row=0, column=0, columnspan=5, sticky=NSEW)
@@ -74,10 +99,17 @@ bt2.grid(row=3, column=1, sticky=NSEW)
 bt3.grid(row=3, column=2, sticky=NSEW)
 bt0.grid(row=4, column=0, sticky=NSEW)
 bt10.grid(row=4, column=1, sticky=NSEW)
+
+
 bt11.grid(row=1, column=3, sticky=NSEW)
 bt12.grid(row=2, column=3, sticky=NSEW)
 bt13.grid(row=3, column=3, sticky=NSEW)
 bt14.grid(row=4, column=3, sticky=NSEW)
 bt15.grid(row=4, column=2, sticky=NSEW)
+
+bt16.grid(row=5, column=0, sticky=NSEW)
+bt17.grid(row=5, column=1, sticky=NSEW)
+bt18.grid(row=5, column=2, sticky=NSEW)
+bt19.grid(row=5, column=3, sticky=NSEW)
 
 janela.mainloop()
