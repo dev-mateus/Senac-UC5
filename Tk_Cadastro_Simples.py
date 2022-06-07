eac813d28fa41e10a1aa8d65192a7d7af0da73ed
mateus <mateus.pereira4@gmail.com>
2022-06-06 21:37:29 -0300

    Exemplo ficha de cadastro 'Dados Pessoais' e 'Endereço'

--- Tk_Cadastro_Simples.py
+++ Tk_Cadastro_Simples.py
@@ -1,26 +1,70 @@
 from tkinter import *
 root = Tk()
+root.title('cadastro')
 
-fr1 = Frame(root)
-fr2 = Frame(root)
+fr1 = Frame(root, padx=10, pady=10)
+fr2 = Frame(root, padx=10, pady=10)
+fr3 = Frame(root, padx=10, pady=10)
 
-lb1 = Label(fr1, text='Nome:', font='Arial 16 bold', anchor=E)
-lb2 = Label(fr1, text='Sobrenome:', font='Arial 16 bold', anchor=E)
-en1 = Entry(fr1, font='Arial 16 bold')
-en2 = Entry(fr1, font='Arial 16 bold')
-lb3 = Label(fr1, text='Sexo:', font='Arial 16 bold',  anchor=E)
-cb1 = Checkbutton(fr1, text='Masculino', font='Arial 16 bold')
-cb2 = Checkbutton(fr1, text='Feminino', font='Arial 16 bold')
+# ########################## widgets frame 1 ########################## #
+lb1_fr1 = Label(fr1, text="Dados Pessoais", font='Verdana 13 italic', anchor=W, fg='blue', pady=3)
+lb2_fr1 = Label(fr1, text='Nome:', font='Verdana 12', anchor=E, pady=3)
+lb3_fr1 = Label(fr1, text='Data Nasc:', font='Verdana 12', anchor=E, pady=3)
+lb4_fr1 = Label(fr1, text='CPF:', font='Verdana 12', anchor=E, pady=3)
+lb5_fr1 = Label(fr1, text='Telefone:', font='Verdana 12',  anchor=E, pady=3)
+en1_fr1 = Entry(fr1, font='Verdana 12 bold')
+en2_fr1 = Entry(fr1, font='Verdana 12 bold')
+en3_fr1 = Entry(fr1, font='Verdana 12 bold')
+en4_fr1 = Entry(fr1, font='Verdana 12 bold')
 
-fr1.grid(row=0, column=0)
-fr2.grid(row=1, column=0)
+# ########################## widgets frame 2 ########################## #
+lb1_fr2 = Label(fr2, text="Endereço", font='Verdana 13 italic', anchor=W, fg='blue', pady=3)
+lb2_fr2 = Label(fr2, text='Rua:', font='Verdana 12', anchor=E, pady=3)
+lb3_fr2 = Label(fr2, text='N°:', font='Verdana 12', anchor=E, pady=3)
+lb4_fr2 = Label(fr2, text='Bairro:', font='Verdana 12', anchor=E, pady=3)
+lb5_fr2 = Label(fr2, text='Cidade:', font='Verdana 12',  anchor=E, pady=3)
+lb6_fr2 = Label(fr2, text='UF:', font='Verdana 12',  anchor=E, pady=3)
+en1_fr2 = Entry(fr2, font='Verdana 12 bold')
+en2_fr2 = Entry(fr2, font='Verdana 12 bold', width=6)
+en3_fr2 = Entry(fr2, font='Verdana 12 bold')
+en4_fr2 = Entry(fr2, font='Verdana 12 bold')
+en5_fr2 = Entry(fr2, font='Verdana 12 bold', width=6)
 
-lb1.grid(row=0, column=0, sticky=EW)
-lb2.grid(row=1, column=0, sticky=EW)
-en1.grid(row=0, column=1, columnspan=2)
-en2.grid(row=1, column=1, columnspan=2)
-lb3.grid(row=2, column=0, sticky=EW)
-cb1.grid(row=2, column=1)
-cb2.grid(row=2, column=2)
+# ########################## widgets frame 3 ########################## #
+bt1_fr3 = Button(fr3, text='Gravar Dados')
+bt2_fr3 = Button(fr3, text='Listar Dados')
+
+# ########################### layout frames ########################### #
+fr1.pack(fill=BOTH, anchor=W)
+fr2.pack(fill=BOTH, anchor=W)
+fr3.pack(fill=BOTH, anchor=NE)
+
+# ####################### layout widgets frame 1 ####################### #
+lb1_fr1.grid(row=0, column=0, columnspan=2, sticky=EW)
+lb2_fr1.grid(row=1, column=0, sticky=EW)
+lb3_fr1.grid(row=2, column=0, sticky=EW)
+lb4_fr1.grid(row=3, column=0, sticky=EW)
+lb5_fr1.grid(row=4, column=0, sticky=EW)
+en1_fr1.grid(row=1, column=1)
+en2_fr1.grid(row=2, column=1)
+en3_fr1.grid(row=3, column=1)
+en4_fr1.grid(row=4, column=1)
+
+# ####################### layout widgets frame 2 ####################### #
+lb1_fr2.grid(row=0, column=0, columnspan=2, sticky=EW)
+lb2_fr2.grid(row=1, column=0, sticky=EW)
+lb3_fr2.grid(row=1, column=4, sticky=EW)
+lb4_fr2.grid(row=2, column=0, sticky=EW)
+lb5_fr2.grid(row=2, column=2, sticky=EW)
+lb6_fr2.grid(row=2, column=4, sticky=EW)
+en1_fr2.grid(row=1, column=1, columnspan=3, sticky=EW)
+en2_fr2.grid(row=1, column=5)
+en3_fr2.grid(row=2, column=1)
+en4_fr2.grid(row=2, column=3)
+en5_fr2.grid(row=2, column=5)
+
+# ####################### layout widgets frame 3 ####################### #
+bt1_fr3.grid(row=0, column=0, sticky=EW)
+bt2_fr3.grid(row=0, column=1, sticky=EW)
 
 root.mainloop()
